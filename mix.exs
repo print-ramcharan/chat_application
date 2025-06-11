d3e6a400f826a96a1f46942ea7ef0cf859f7b561
print-ramcharan <ramcharanpolabathina@gmail.com>
2025-06-12 02:29:10 +0530

    removed database url

--- mix.exs
+++ mix.exs
@@ -29,6 +29,7 @@ defmodule WhatsappClone.MixProject do
       {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
+      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:swoosh, "~> 1.5"},
       {:finch, "~> 0.13"},
