b777d27e12823a7a620817a7adf3e52cf3495cf1
print-ramcharan <ramcharanpolabathina@gmail.com>
2025-06-12 02:28:31 +0530

    removed database url

--- mix.exs
+++ mix.exs
@@ -49,6 +49,7 @@ defmodule WhatsappClone.MixProject do
   defp aliases do
     [
       setup: ["deps.get", "ecto.setup"],
+      "assets.deploy": ["esbuild default --minify", "phx.digest"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
