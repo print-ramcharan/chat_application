0c65fa835b6d254753a8f59c724f2f63cce15da3
print-ramcharan <ramcharanpolabathina@gmail.com>
2025-07-01 01:50:02 +0530

    updated the server startup by adding verification endpoint and added access, refresh tokens logic

--- config/config.exs
+++ config/config.exs
@@ -52,7 +52,7 @@ config :whatsapp_clone, WhatsappCloneWeb.Endpoint,
   # json: File.read!("config/firebase-service-account.json")
 
 
-  # config :goth, json: System.get_env("FIREBASE_CREDENTIALS_JSON")
+  config :goth, json: System.get_env("FIREBASE_CREDENTIALS_JSON")
 
   # config :goth, json: File.read!("config/firebase-service-account.json")
 
