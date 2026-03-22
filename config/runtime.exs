0e710c07293c965fc6f0453e0d771eef57072d3f
print-ramcharan <ramcharanpolabathina@gmail.com>
2026-03-22 21:50:47 +0530

    fix: disable ssl verification for db

--- config/runtime.exs
+++ config/runtime.exs
@@ -23,6 +23,7 @@ if config_env() == :prod do
 
   config :whatsapp_clone, WhatsappClone.Repo,
     ssl: true,
+    ssl_opts: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
